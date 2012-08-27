aaf8cda3307a55d74b2de67bc1daf8ef3982bcac
bryan kennedy <bkennedy@smm.org>
2012-08-27 16:46:25 -0500

    Initial Drupal fabric task
    Starting a seperate repo call Neoprene which will contain public Drupal
    tasks for use with the Fabric deploy system. This first one does a
    Backup and Migrate database dump.

---
+++ drupal.py
@@ -0,0 +1,16 @@
+def bam_backup(directory):
+    """
+    Uses drush and the Backup and Migrate modual to make sql dump of the
+    database for the spcified site.
+    """
+    with cd(directory):
+        output = run("drush bam-backup")
+        # Get the backup filename from the response
+        r = re.match('Default Database backed up successfully to '
+                      '([^\s]*) '
+                      'in destination Manual Backups Directory in '
+                      '[\d]*\.[\d]* ms\.[\s]*\[success\]',
+                      output
+                    )
+        backup_file = r.group(1)
+
