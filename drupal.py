f9ab8119de73c27a75c799d6cd5d232d5bccb546
bryan kennedy <bkennedy@smm.org>
2012-08-27 17:52:48 -0500

    Get the full filepath to the backup

--- drupal.py
+++ drupal.py
@@ -2,17 +2,24 @@ def db_backup(directory):
     """
     Uses drush and the Backup and Migrate modual to make sql dump of the
     database for the spcified site.
+
+    Returns the full filepath to the backup
     """
     with cd(directory):
-        output = run("drush bam-backup")
+        o = run("drush bam-backup")
         # Get the backup filename from the response
         r = re.match('Default Database backed up successfully to '
                       '([^\s]*) '
                       'in destination Manual Backups Directory in '
                       '[\d]*\.[\d]* ms\.[\s]*\[success\]',
-                      output
+                      o
                     )
-        backup_file = r.group(1)
+        file = r.group(1) ".mysql.gz"
+        o = run("drush vget file_private_path")
+        r = re.match('file_private_path: "([^"]*)"', o)
+        path = r.group(1) "/backup_migrate/manual/"
+        filepath = directory path file
+        return filepath
 
 def clear_cache():
     """
