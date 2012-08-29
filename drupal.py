691caa43e04c4af7de02f148b98bf8197ee3ca75
bryan kennedy <bkennedy@smm.org>
2012-08-29 17:42:26 -0500

    Improved way of finding the backup filepath
    This will work regardless of whether the user inputs the trailing slash
    on the directory name input argument or not. Makes it a bit less
    fragile.

--- drupal.py
+++ drupal.py
@@ -56,12 +56,14 @@ def remote_db_dump(directory):
                      '[\d]*\.[\d]* ms.',
                      r
                     )
-        file = r.group(1) + ".mysql.gz"
-        o = run("drush vget file_private_path")
-        r = re.match('file_private_path: "([^"]*)"', o)
-        path = r.group(1) + "/backup_migrate/manual/"
-        filepath = directory + path + file
-        return filepath
+        filename = r.group(1) + ".mysql.gz"
+        file_private_path = run("drush vget file_private_path")
+        # Remove quotes around the variable
+        r = re.match('file_private_path: "([^"]*)"', file_private_path)
+        bam_path = r.group(1) + "/backup_migrate/manual/"
+        with cd(bam_path):
+            bam_filepath = env.cwd + file
+            return bam_filepath
 
 @task
 def get_db_dump(directory):
