22c5386027d33f37a21b6a771f1f1b8ad66d655f
bryan kennedy <bkennedy@smm.org>
2013-04-30 15:56:14 -0500

    Moving helper functions into their own module

--- __init__.py
+++ __init__.py
@@ -1,76 +1,16 @@
 # Fabric modules
-from fabric.api import (cd, env, hide, run, settings, task)
+from fabric.api import (cd, env, run, task)
 from fabric.contrib.files import (exists)
-from fabric.contrib.console import (confirm)
-from fabric.colors import blue, red, green
+from fabric.colors import green
 
 # Neoprene modules
 import cache
 import db
 import files
 
-# Python modules
-from contextlib import contextmanager
-import re
-
 
 # Source your local environment variables to make Drush work properly
 env.shell = "/bin/bash -l -c -i"
-
-
-@contextmanager
-def _mute():
-    """Run a fabric command without reporting any responses to the user. """
-    with settings(warn_only='true'):
-        with hide('running', 'stdout', 'stderr', 'warnings'):
-            yield
-
-
-def _header(txt):
-    """Decorate a string to make it stand out as a header. """
-    wrapper = "------------------------------------------------------"
-    return blue(wrapper + "\n" + txt + "\n" + wrapper, bold=True)
-
-
-def _cleanup_drush_output(o):
-    """Cleanup drush output to get raw message string.
-
-    This is useful when you need to run a regex against a response string
-    and don't want to deal with any of the special formatting and linebreaks
-    that Drush can return
-
-    Args:
-        o: Drush output string
-
-    Returns:
-        Raw response string
-    """
-    # Remove linebreaks
-    r = ' '.join(o.splitlines())
-    # Remove Drush decoration which can show up anywhere in the message
-    r = re.sub('\[success\]', '', r)
-    # Remove extraneous white space
-    r = re.sub('\s+', ' ', r)
-    return r
-
-
-def confirm_overwrite(warning):
-    """Prompt the user with a noticable warning before overwriting files
-
-    Args:
-        warning: Warning string to be presented to the user. This should
-            include the question string, but not the [y/N] bit. That's
-            added by Fabric's confirm function. Defaults to No.
-
-    Returns:
-        Quits all operations if the user says No.
-        Continues operations with a pass if the user says Yes.
-    """
-    answer = confirm(red(warning), default=False)
-    if answer is not True:
-        exit('Quiting')
-    else:
-        pass
 
 
 @task
